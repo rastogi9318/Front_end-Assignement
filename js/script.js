5b0deeb4ecb9b02e382f01972ebfb7f6708613fb
Yash Rastogi <70816945+rastogi9318@users.noreply.github.com>
2022-07-11 21:03:09 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+      nextEl: ".arrowCircle",
+      prevEl: ".arrow1",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
